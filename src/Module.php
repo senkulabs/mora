ef2589c7d27bbf35c279227859fcf6b51e384ea5
Kresna Satya <kresnasatya72@gmail.com>
2026-01-02 13:33:29 +0800

    refactor: remove comments

--- src/Module.php
+++ src/Module.php
@@ -7,24 +7,12 @@ use Illuminate\Support\Str;
 
 class Module
 {
-    /**
-     * The application instance.
-     */
     protected Container $app;
 
-    /**
-     * The module name.
-     */
     protected string $name;
 
-    /**
-     * The module path.
-     */
     protected string $path;
 
-    /**
-     * The constructor.
-     */
     public function __construct(Container $app, string $name, string $path)
     {
         $this->app = $app;
@@ -32,57 +20,36 @@ class Module
         $this->path = $path;
     }
 
-    /**
-     * Get name.
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * Get name in lower case.
-     */
     public function getLowerName(): string
     {
         return strtolower($this->name);
     }
 
-    /**
-     * Get name in studly case.
-     */
     public function getStudlyName(): string
     {
         return Str::studly($this->name);
     }
 
-    /**
-     * Get name in kebab case.
-     */
     public function getKebabName(): string
     {
         return Str::kebab($this->name);
     }
 
-    /**
-     * Get path.
-     */
     public function getPath(): string
     {
         return $this->path;
     }
 
-    /**
-     * Get extra path.
-     */
     public function getExtraPath(?string $path = null): string
     {
         return $this->getPath().($path ? '/'.$path : '');
     }
 
-    /**
-     * Handle call __toString.
-     */
     public function __toString(): string
     {
         return $this->getStudlyName();
